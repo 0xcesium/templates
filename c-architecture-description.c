8e9dd0a0632d510f665c2877df7d5128bb928bcb
Cesium <ilal.jim@gmail.com>
2017-10-16 10:54:57 +0200

    Update c-architecture-description.c

--- c-architecture-description.c
+++ c-architecture-description.c
@@ -7,14 +7,14 @@
  */
 
 
-int bss1; 					  /* Bss Segment */
-int ds1 = 10;					/* stored in Data Segment */
+int bss1; 				/* Bss Segment */
+int ds1 = 10;				/* stored in Data Segment */
 
 int main(int argc, char *argv[]) {
 
-	static int bss2; 			    /* Bss Segment */
+	static int bss2; 		/* Bss Segment */
 	static int ds2 = 100; 		/* Data Segment */
-	int stack = 200;			    /* stack var */
+	int stack = 200;		/* stack var */
 	char *heap = malloc(10);	/* heap var */
 
 	return 0;
