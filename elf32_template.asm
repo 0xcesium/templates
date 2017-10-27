01f6442c1db2460d112c520a8f91c8a670d935f0
Cesium <ilal.jim@gmail.com>
2017-10-27 10:03:04 +0200

    Update elf32_template.asm

--- elf32_template.asm
+++ elf32_template.asm
@@ -6,37 +6,37 @@ BITS 32
 ehdr:         ; Elf32_Ehdr
   db      0x7F, "ELF", 1, 1, 1, 0	;   e_ident
   times 8 db      0
-  dw      2   ;   e_type
-  dw      3   ;   e_machine
-  dd      1   ;   e_version
-  dd      _start ;   e_entry
+  dw      2         ;   e_type
+  dw      3         ;   e_machine
+  dd      1         ;   e_version
+  dd      _start    ;   e_entry
   dd      phdr - $$ ;   e_phoff
-  dd      0	    ;   e_shoff
-  dd      0	    ;   e_flags
+  dd      0	        ;   e_shoff
+  dd      0	        ;   e_flags
   dw      ehdrsize  ;   e_ehsize
   dw      phdrsize  ;   e_phentsize
-  dw      1	    ;   e_phnum
-  dw      0	    ;   e_shentsize
-  dw      0	    ;   e_shnum
-  dw      0	    ;   e_shstrndx
+  dw      1	        ;   e_phnum
+  dw      0	        ;   e_shentsize
+  dw      0	        ;   e_shnum
+  dw      0	        ;   e_shstrndx
 
 ehdrsize      equ     $ - ehdr
 
-phdr:				  ; Elf32_Phdr
-  dd      1   ;   p_type
-  dd      0   ;   p_offset
-  dd      $$  ;   p_vaddr
-  dd      $$  ;   p_paddr
-  dd      filesize ;   p_filesz
-  dd      filesize ;   p_memsz
-  dd      5	  ;   p_flags
-  dd      0x1000	 ;   p_align
+phdr:				        ; Elf32_Phdr
+  dd      1         ;   p_type
+  dd      0         ;   p_offset
+  dd      $$        ;   p_vaddr
+  dd      $$        ;   p_paddr
+  dd      filesize  ;   p_filesz
+  dd      filesize  ;   p_memsz
+  dd      5	        ;   p_flags
+  dd      0x1000	  ;   p_align
 
 phdrsize      equ     $ - phdr
 
 _start:
-  mov al, 1     	; sys_exit
-  mov bl, 42    	; int status
+  mov al, 1     	  ; sys_exit
+  mov bl, 42    	  ; int status
   int 0x80
 
 filesize      equ     $ - $$
